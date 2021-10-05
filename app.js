f7fa9b9e6793b12836ac34fbdf381215df3fa088
Mark Voltaire <markjvoltaire@gmail.com>
2021-10-05 10:26:01 -0400

    number input works

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
 // import functions and grab DOM elements
+const entry = document.getElementById('entry')
+const guessButton = document.getElementById('submitEntry')
+const reload = document.getElementById('tryAgain')
+const guessCounter = document.getElementById('guessCount')
+const resultText = document.getElementById('results')
 
 // initialize global state
+let numbGuess = 4;
+const randomNumber = Math.floor(Math.random()* 20) + 1;
+
 
 // set event listeners 
+guessButton.addEventListener('click', () => {
+  const userGuess = Number(entry.value);
+  console.log(Number(entry.value));
+})
+
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 
 
 
-  const button = document.getElementById('guessButton')
-const inputResults = document.getElementById('results')
-const entry = document.getElementById('results')
-const guessCount = document.getElementById('')
-
-const randomNumber = () => {
-    return Math.ceil(Math.random() *20 )
-}
-
-let guessesLeft = 4;
-
-button.addEventListener('click', ()=>{
-   const guess = Number(entry.value);
-   guessesLeft-- ;
-   let inputMessage;
-   if(guess === randomNumber){
-       inputMessage = 'You Win'
-   } else if (guessesLeft === 0) {
-     inputMessage = 'Sorry Try Again';
-     button.style.display = 'none';
-
-   }
-
+ 
     
-})
+
 
